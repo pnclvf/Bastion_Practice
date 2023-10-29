e72144c3651e094ad5794162e9027ef7d4e915b2
pnclvf <pnclvf@gmail.com>
2023-10-29 16:05:37 -0700

    first commit

---
+++ terraform_ansible_generic_instace_setup_bastion/instances.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "bastion_1" {
+  ami                    = "ami-053b0d53c279acc90" # Ubuntu server 22.04
+  instance_type          = "t2.micro" # Do not forget to turn off the instance after the test (56$ per month)
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "secGroupLesson2"
+  iam_instance_profile   = "AWS-Terraform"
+
+  tags = {
+    Name = "Bastion_1"
+  }
+}
